a07485f3ef571f760ac102596cb4acc8764889c0
Melissa Longenberger <apple@Irish-iMac.hitronhub.home>
2022-04-11 15:26:09 -0400

    changes to week1 file to work on windows

--- week1/day2/index.js
+++ week1/day2/index.js
@@ -4,36 +4,37 @@ var page = url.parse('https://github.com/InstructorMelissa-KnowledgeHut/fsdr-fas
 // console.log("display host: ", page.host) // This will display the domain name or root url address
 // console.log("display pathname: ", page.pathname) // This will display the current url's folder page location with in the url
 
-// console.log(process.cwd())
-// var input = process.stdin
-// input.setEncoding('utf-8')
-// input.on('data', (data)=> {
-//     if (data =='exit\n') {
-//         console.log('Thank you') //If we type in the terminal exit and hit enter then this statement will run
-//         process.exit()
-//     }
-//     else if (data =='Hello\n') {
-//         console.log("Why hello there") // If we type Hello this statement will run
-//         process.exit()
-//     } else {
-//         process.exit()  // Anything that is typed that is not covered by the if statements will trigger this to run
-//     }
-// })
-
-function palindrome(word) {
-    const cleanUp = word.toLowerCase().replace(/[\W_]/g, "")
-    let fixedWord = ""
-    for (char of cleanUp) {
-        fixedWord = char + fixedWord
+console.log(process.cwd())
+var input = process.stdin
+input.setEncoding('utf-8')
+input.on('data', (data)=> {
+    data = data.trim()
+    if (data =='exit') {
+        console.log('Thank you') //If we type in the terminal exit and hit enter then this statement will run
+        process.exit()
     }
-    console.log("the fixedword: ", fixedWord)
-    if (fixedWord === cleanUp) {
-        console.log("It's a palindrome")
+    else if (data =='Hello') {
+        console.log("Why hello there") // If we type Hello this statement will run
+        process.exit()
     } else {
-        console.log("Not a palindrome")
+        process.exit()  // Anything that is typed that is not covered by the if statements will trigger this to run
     }
-    
-}
+})
 
-palindrome('Mom')
-palindrome('Dog')
+// function palindrome(word) {
+//     const cleanUp = word.toLowerCase().replace(/[\W_]/g, "")
+//     let fixedWord = ""
+//     for (char of cleanUp) {
+//         fixedWord = char + fixedWord
+//     }
+//     console.log("the fixedword: ", fixedWord)
+//     if (fixedWord === cleanUp) {
+//         console.log("It's a palindrome")
+//     } else {
+//         console.log("Not a palindrome")
+//     }
+    
+// }
+
+// palindrome('Mom')
+// palindrome('Dog')
